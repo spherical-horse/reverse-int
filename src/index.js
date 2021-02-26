23316bfbee26ac4b8eb311c02b9bc9910842125e
Artem <tema.shevchuk@gmail.com>
2021-02-26 19:40:01 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    const arr = n.toString().split('');
+    return parseInt(arr.reverse().join(''));
 }
